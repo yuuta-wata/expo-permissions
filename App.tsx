81380d97dad7c6f07489857e4706848909e6a191
Yuuta Watanabe <yuuta@Yuuta.local>
2022-08-27 16:12:42 +0900

    permissions standard setting

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Linking, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {useForegroundPermissions, PermissionStatus, getCurrentPositionAsync} from 'expo-location'
+import {startActivityAsync, ActivityAction} from 'expo-intent-launcher'
 
 export default function App() {
+  const [permissions, requestPermissions] = useForegroundPermissions()
+
+  const {DENIED, GRANTED, UNDETERMINED} = PermissionStatus
+  const {LOCATION_SOURCE_SETTINGS} = ActivityAction
+
+  const openAppSettingsPage = () => {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('app-settings:')
+    }
+
+    if (Platform.OS === 'android') {
+      startActivityAsync(LOCATION_SOURCE_SETTINGS)
+    }
+  }
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>permissions states: {permissions?.status}</Text>
+      <TouchableOpacity style={styles.permissionButton} onPress={async () => {
+        const {status} = await requestPermissions()
+        console.log('status:', status);
+        
+        if (status === DENIED) {
+          Alert.alert('位置情報を取得しませんでした。', '再度設定するには設定画面へ移動してください。', [
+            {text: 'キャンセル', style: 'cancel'},
+            {text: '移動する', onPress: openAppSettingsPage}
+          ])
+          return;
+        }
+
+        const location = await getCurrentPositionAsync()
+        console.log('location:', location);     
+      }}>
+        <Text>Permission</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +51,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  permissionButton: {
+    backgroundColor: 'pink',
+    padding: 20
+  }
 });
